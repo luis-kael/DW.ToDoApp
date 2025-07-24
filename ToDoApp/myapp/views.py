bf6de148b0453d6ec319674f8cb95723a0c89338
luis-kael <sithukaungsett2001@gmail.com>
2025-07-24 20:19:32 +0630

    css

--- ToDoApp/myapp/views.py
+++ ToDoApp/myapp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from .models import TodoModel
 # Create your views here.
+
+def ToDoList(request):
+    todo = TodoModel.objects.all
+    contex = {
+        'todo ' : todo
+    }
+    return render(request, 'index.html', contex) 
